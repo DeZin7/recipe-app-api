f1713b2eceb8010b9ffdf933654dd38fc1912561
DeZin7 <marcusandre77@icloud.com>
2023-03-04 13:38:57 -0800

    Implementing manage user API.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -122,7 +122,7 @@ class PrivateUserApiTest(TestCase):
     def setUp(self):
         self.user = create_user(
             email='test@example.com',
-            passwoer='testpass123',
+            password='testpass123',
             name='Test Name',
         )
         self.client = APIClient()
@@ -154,4 +154,3 @@ class PrivateUserApiTest(TestCase):
         self.assertEqual(self.user.name, payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-
