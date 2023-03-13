5389ddd14f0b1baebe6dee058b6b5698ad002fb4
DeZin7 <marcusandre77@icloud.com>
2023-03-13 10:49:52 -0700

    Implementing update recipe tags feature.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -271,7 +271,7 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn(tag_lunch, recipe.tags.all())
-        self.assertNotIn(tag_breakfast, recipe.tag.all())
+        self.assertNotIn(tag_breakfast, recipe.tags.all())
 
     def test_clear_recipe_tags(self):
         """Test clearing a recipe tags."""
@@ -281,7 +281,7 @@ class PrivateRecipeApiTests(TestCase):
 
         payload = {'tags': []}
         url = detail_url(recipe.id)
-        res = self.client.patch(url, payload, foramt='json')
+        res = self.client.patch(url, payload, format='json')
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.tags.count(), 0)
